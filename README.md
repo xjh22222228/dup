a0e121826cbb0a0864ec0146a49b59a4fcb54d47
xjh22222228 <xjh22222228@gmail.com>
2020-11-14 15:48:39 +0800

    fix: print total

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
----
-过滤重复文本内容
----
+
+<p align="center">
+  <b>dup 是一个用来过滤重复文本内容</b>
+  <p align="center">
+    <img src="https://img.shields.io/github/license/xjh22222228/nav" />
+    <img src="https://img.shields.io/github/v/release/xjh22222228/dup" />
+  </p>
+</p>
+
+![](media/print.png)
+
+
 
 ## Install
 ```
@@ -10,15 +19,19 @@ git clone --depth=1 https://github.com/xjh22222228/dup.git
 
 ## Usage
 - filein 指定需要过滤文本路径
-- fileout 指定过滤后输出路径，默认当前下的 out.txt
+- fileout 可选, 指定过滤后输出路径，默认当前下的 out.txt
 
-```
+```bash
 python3 dup.py --filein=./example.txt
 
 python3 dup.py --filein=./example.txt --fileout=./out.txt
 ```
 
+文本需要是 `utf-8` 编码
+
+
+
 
 ## License
-
+MIT
 
--- dup.py
+++ dup.py
@@ -1,8 +1,9 @@
 from fire import Fire
-import sys
-import os
+import sys, os, time
+from colorama import init, Fore
 
 lineMap = {}
+start_timestamp = time.time()
 
 
 def parse_command(filein='./example.txt', fileout='./out.txt'):
@@ -17,12 +18,14 @@ def parse_command(filein='./example.txt', fileout='./out.txt'):
     except FileNotFoundError as e:
         pass
 
-    with open(abspath_filein) as filein_obj:
+    with open(abspath_filein, encoding='utf-8') as filein_obj:
         if not filein_obj.readable():
             print('当前文件无权限读取')
             sys.exit(0)
 
-        for lineVal in filein_obj.readlines():
+        lines = filein_obj.readlines()
+        before_total = len(lines)
+        for lineVal in lines:
             if lineVal != '':
                 lineMap[lineVal] = True
 
@@ -32,8 +35,16 @@ def parse_command(filein='./example.txt', fileout='./out.txt'):
             sys.exit(0)
 
         for lineVal in lineMap.keys():
-            print(fileout_obj.write(lineVal))
+            fileout_obj.write(lineVal)
+
+    after_total = len(lineMap.keys())
+    end_timestamp = time.time()
+
+    print('Before Line: {}, After Line: {}'.format(str(before_total), str(after_total)))
+    print('Total: {} duplicates'.format(str(before_total - after_total)))
+    print(Fore.GREEN + 'Time: ' + str(round(end_timestamp - start_timestamp, 5)) + ' s')
 
 
 if __name__ == '__main__':
+    init()
     Fire(parse_command)
